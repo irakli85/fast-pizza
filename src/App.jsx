8a14da74ab9d17929d4de33099acac387ec9b51e
irakli85 <iberidze38@gmail.com>
2024-04-09 19:04:11 +0400

    285

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import Home from "./ui/Home"
-import Menu from "./features/manu/Menu"
+import Error from "./ui/Error"
+import Menu, {loader as menuLoader} from "./features/manu/Menu"
 import Cart from "./features/cart/Cart"
-import CreateOrder from "./features/order/CreateOrder"
-import Order from "./features/order/Order"
+import CreateOrder, {action as createOrderAction} from "./features/order/CreateOrder"
+import Order, { loader as orderLoader } from "./features/order/Order"
 import Applayout from "./ui/Applayout"
 
 
 const router = createBrowserRouter([
   { 
     element: <Applayout/>,
+    errorElement: <Error/>,
     children: [
       {path: '/', element: <Home/>},
-      {path: '/menu', element: <Menu/>},
+      {path: '/menu', element: <Menu/>, loader: menuLoader,  errorElement: <Error/>},
       {path: '/cart', element: <Cart/>},
-      {path: '/order/new', element: <CreateOrder/>},
-      {path: '/order/:oderId', element: <Order/>}
+      {path: '/order/new', element: <CreateOrder/>, action: createOrderAction},
+      {path: '/order/:orderId', element: <Order/>, loader: orderLoader, errorElement: <Error/> }
     ]
   }
 ])
